226c1f2f49b3363b481723965f142e557f167f5e
Karaskaki <nik.ranchi7@gmail.com>
2024-03-16 09:01:57 +0530

    installed all modules

---
+++ twitter-client/pages/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid grid-cols-12 h-screen w-screen">
+        <div className="col-span-3"></div>
+        <div className="col-span-6 border border-r-[1px] border-l-[1px] border-grey-400"></div>
+        <div className="col-span-3"></div>
+      </div>
+    </div>
+  );
+}

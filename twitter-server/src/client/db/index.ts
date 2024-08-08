d29d72d5c1e4c036e74a16e8d3a9efcca1def23d
Dipesh Kant <dipeshkant0@gmail.com>
2024-08-08 17:18:39 +0530

    Added hello

--- twitter-server/src/client/db/index.ts
+++ twitter-server/src/client/db/index.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prismaClient = new PrismaClient({ log: ["query"] })
+export const syyHello = ()=>{
+     console.log("Hello")
+}
+//hello nikhil kumar singh 
